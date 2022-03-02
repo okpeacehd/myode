bbe9f4e5b947f05fc6af8795908ac8fe85ab74c4
okpeacehd <100624736+okpeacehd@users.noreply.github.com>
2022-03-02 12:01:26 +0000

    Update Trial.m

--- Trial.m
+++ Trial.m
@@ -5,20 +5,20 @@ tspan = [0,45000];       % time span
 C0 = [1,0,0,0,0,0,0,0,0,1,0];  % initial concentrations for main
 %% ODE Solver
 [t,C] = ode45(@InternalODE,tspan,C0);
+plot(t,C);
 %% Rh
-
+%constants
 df=1.8;
 mwM=150e3;
 A = 5.4./(mwM.^(1/df));
 n = size(C,2)-2;
-
+% initials
 n1 = (C(:,1).*(7.5).*((mwM).^2))+(C(:,2).*(5.4).*((mwM).^2));
 d1 = (C(:,1).*mwM.^2)+(C(:,2).*mwM.^2);
-
+%vectorised loop
 denominator = d1 + sum(((C(:,(3:n))).*((((3:n)-1).*mwM).^2)),2);
-numerator = n1 + sum((C(:,(3:n)).*A.*(((3:n)-1).*mwM).^(1/df).*((((3:n)-1).*mwM).^2)),2);
+numerator = n1 + sum((C(:,(3:n)).*A.*((((3:n)-1).*mwM).^(1/df)).*((((3:n)-1).*mwM).^2)),2);
 Rh = numerator./denominator;
-     
 
 figure(2)
 plot(t,Rh);
@@ -47,36 +47,44 @@ P = @(i, j) (i*j).^L;                 % Product kernel (additional Factor)
 k = @(i, j) (ks./W) .* B(i,j) ...     % Aggregation rate via DLCA
     .* P(i,j);
 %% Rate of Aggregation 
+x=9;
+for i = 2:x
+    for j = 2:x
+        if i==j
+            kArray(i,j) = 2.*k(i-1,j-1).*C(i).*C(j);
+            bArray(i,j) = k(i-1,j-1).*C(i).*C(j);
+        else 
+            kArray(i,j) = k(i-1,j-1).*C(i).*C(j);
+            bArray(i,j) = k(i-1,j-1).*C(i).*C(j);
+        end
+    end
+end
+     
 % Monomer unfolded
 unfolded = -kf.*C(1)+kuf.*C(2);
 % Monomer folded
-folded =   kf.*C(1)-kuf.*C(2)-2.*(C(2)^2).*k(1,1)-(C(2).*C(3)).*k(1,2)-(C(2).*C(4))...
-    .*k(1,3)-(C(2).*C(5)).*k(1,4)-(C(2).*C(6)).*k(1,5)-(C(2).*C(7)).*k(1,6)-(C(2).*C(8)).*k(1,7);
+folded =  kf.*C(1)-kuf.*C(2)-sum(kArray(2,2:x-1));
+
 % Dimer    (2)
-dimer =    (C(2)^2).*k(1,1)-(C(2).*C(3)).*k(1,2)-2.*(C(3).*C(3)).*k(2,2)-(C(4).*C(3))...
-    .*k(3,2)-(C(5).*C(3)).*k(4,2)-(C(6).*C(3)).*k(5,2)-(C(7).*C(3)).*k(6,2);
+dimer =    (C(2)^2).*k(1,1)-sum(kArray(3,2:x-2));
 % Trimer   (3)
-trimer =   (C(2).*C(3)).*k(1,2)-(C(2).*C(4)).*k(1,3)-(C(3).*C(4)).*k(2,3)-2.*...
-    (C(4).*C(4)).*k(3,3)-(C(5).*C(4)).*k(4,3)-(C(6).*C(4)).*k(5,3);
+trimer =   (C(2).*C(3)).*k(1,2)-sum(kArray(4,2:x-3));
 % Tetramer (4)
-tetramer = (C(2).*C(4)).*k(1,3)+(C(3)^2).*k(2,2)-(C(2)*C(5).*k(1,4))-(C(3).*C(5).*k(2,4))...
-    -(C(4).*C(5).*k(3,4))-2.*(C(5).*C(5).*k(4,4));
+tetramer = sum((bArray(2:3,4:-1:3)))-sum(kArray(5,2:x-4));
 % pentamer (5)
-pentamer = (C(2).*C(5)).*k(1,4)+(C(3).*C(4).*k(2,3))-(C(2).*C(6).*k(1,5))-(C(3).*...
-    C(6).*k(2,5))-(C(4).*C(6).*k(3,5));
+pentamer = sum((bArray(2:3,5:-1:4)))-sum(kArray(6,2:x-5));
 % hexamer  (6)
-hexamer =  (C(2).*C(6)).*k(1,5)+(C(3).*C(5).*k(2,4))+(C(4).*C(4).*k(3,3))-(C(2).*...
-    C(7).*k(1,6))-(C(3).*C(7).*k(2,6));
+hexamer =  sum((bArray(2:4,6:-1:4)))-sum(kArray(7,2:x-6));
 % heptamer (7)
-heptamer = (C(2).*C(7)).*k(1,6)+(C(3).*C(6).*k(2,5))+(C(4).*C(5).*k(3,4))-(C(2).*C(8).*k(1,7));
+heptamer = sum((bArray(2:4,7:-1:5)))-sum(kArray(8,2:x-7));
 % octamer  (8)
-octamer =  (C(2).*C(8)).*k(1,7) + (C(3).*C(7).*k(2,6))+(C(4).*C(6).*k(3,5))+(C(5).*C(5).*k(4,4));
+octamer =  sum((bArray(2:5,8:-1:5)));
 
-total = unfolded + folded + 2*dimer + 3*trimer + 4*tetramer + 5*pentamer +...
-    6*hexamer + 7*heptamer + 8*octamer;
+total = unfolded + folded + 2.*dimer + 3.*trimer + 4.*tetramer + 5.*pentamer +...
+    6.*hexamer + 7.*heptamer + 8.*octamer;
 
-aggregated = 2*dimer + 3*trimer + 4*tetramer + 5*pentamer +...
-    6*hexamer + 7*heptamer + 8*octamer;
+aggregated = 2.*dimer + 3.*trimer + 4.*tetramer + 5.*pentamer +...
+    6.*hexamer + 7.*heptamer + 8.*octamer;
 
 dCdt=[unfolded; folded; dimer; trimer; tetramer; pentamer; hexamer; ...
     heptamer; octamer; total; aggregated];
